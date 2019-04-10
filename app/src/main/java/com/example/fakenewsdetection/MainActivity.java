0513b588bb2242242a7fe7df67dda2634ba0a574
Hesham Hosney Elsaye Mohamed <mohamed@ibr.cs.tu-bs.de>
2019-04-10 13:15:12 +0200

    Login Screen

--- app/src/main/java/com/example/fakenewsdetection/MainActivity.java
+++ app/src/main/java/com/example/fakenewsdetection/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.fakenewsdetection;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Variables for activity request in order to track the status for every activity individually and
+    //Track it's exit status
+    private static final int SIGNIN_REQUEST = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Test initial Commit
+        Intent login = new Intent(MainActivity.this, activity_login.class);
+        startActivityForResult(login, SIGNIN_REQUEST);
     }
 }
