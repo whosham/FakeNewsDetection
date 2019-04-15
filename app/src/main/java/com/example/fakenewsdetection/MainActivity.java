f5681515ce3b90e683e95935ec1f72d59c2ccb19
Hesham Hosney Elsaye Mohamed <mohamed@ibr.cs.tu-bs.de>
2019-04-15 13:37:43 +0200

    Logout activity using menu that alllows user to logout. with a confirmation dialog.

--- app/src/main/java/com/example/fakenewsdetection/MainActivity.java
+++ app/src/main/java/com/example/fakenewsdetection/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.fakenewsdetection;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -70,6 +73,43 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id){
+            case R.id.bottom_item_logout:
+                //Logout setting will delete all the shared preferences so the user must relogin
+                Log.d("MainActivity", getString(R.string.Logout));
+                //confirm if the user wanted to logout or not
+                new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.Logout))
+                        .setMessage(getString(R.string.logout_confirmation))
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                //User confirmed loging out
+                                Toast.makeText(MainActivity.this, getString(R.string.Logout), Toast.LENGTH_SHORT).show();
+                                //Delete all his shared preference.
+                                SharedPreferences.Editor editor =
+                                        getSharedPreferences(MY_GLOBAL_PREFS,MODE_PRIVATE).edit();
+                                // Getting Email data from login activity
+                                editor.remove(activity_login.EMAIL_KEY) ;
+                                editor.apply();
+
+                                // Restarting app in case of logout
+                                Intent restart  = getBaseContext().getPackageManager()
+                                        .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+                                restart.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(restart);
+                            }})
+                        .setNegativeButton(android.R.string.no, null).show();
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
