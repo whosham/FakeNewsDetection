6df007846c209162dc6350a468216eaf8023a298
Hesham Hosney Elsaye Mohamed <mohamed@ibr.cs.tu-bs.de>
2019-05-13 11:14:23 +0200

    1-Adding Recycler View for Home-feed activity.

--- app/src/main/java/com/example/fakenewsdetection/MainActivity.java
+++ app/src/main/java/com/example/fakenewsdetection/MainActivity.java
@@ -4,17 +4,21 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -28,7 +32,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton floatingActionButton;
     private BottomAppBar bottomAppBar;
     private Button querycc;
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager ;
+    private CustomAdapter adapter ;
+    private List<MyData> data_list ;
 
 
     //Variables for activity request in order to track the status for every activity individually and
@@ -61,6 +71,35 @@ public class MainActivity extends AppCompatActivity {
             Intent login = new Intent(MainActivity.this, activity_login.class);
             startActivityForResult(login, SIGNIN_REQUEST);
         }
+
+
+
+        //Home Feed //
+//        recyclerView = findViewById(R.id.recyclerView) ;
+//        data_list =  new ArrayList<>() ;
+//        loadDataFromServer(0) ;
+//        //om response add
+//        adapter.notifyDataSetChanged();
+//
+//        gridLayoutManager = new GridLayoutManager(this,2);
+//        recyclerView.setLayoutManager(gridLayoutManager);
+//        //bind data to the recyclerview it self
+//        adapter = new CustomAdapter (this, data_list) ;
+//        recyclerView.setAdapter(adapter);
+//
+//        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+//                if (gridLayoutManager.findLastCompletelyVisibleItemPosition() == data_list.size()-1){
+//                    loadDataFromServer(data_list.get(data_list.size()-1).getId());
+//                }
+//            }
+//        });
+
+
+        
+
+
 
 
 
@@ -118,6 +157,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void loadDataFromServer(int i) {
+
+        //doing the network request fetch json data and
+        // for (int i = - ; i <array.length() ; i++ )  { loiop add to list
+        //Mydata data =  new Mydata(id,desc,imageLink )
+        //data_list.add(data)
+    }
+
     //for handling menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
